d54314c33b06b97109805fc53985c0c893d3af1f
username-matthew <matthew@olk1.com>
2023-05-05 19:01:05 +0100

    --- correct variable namespace mistake LoggedIn + fix gsap onload issues ---

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
-import { gsap, Power1 } from 'gsap';
+import { gsap, Sine } from 'gsap';
 import getSessionData from '@/helpers/getSessionData';
 import CountdownTimer from '@/components/CountdownTimer';
 import LogoutBtn from '@/components/LogoutBtn';
@@ -12,22 +12,31 @@ export default function Home() {
   const [loggedIn, setLoggedIn] = useState(undefined);
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [loadContent, setLoadContent] = useState(false);
 
   const element1Ref = useRef(null);
 
   useEffect(() => {
-  if (element1Ref.current) {
-    gsap.fromTo(
-      element1Ref.current,
-      { opacity: 0, y: 10 },
-      { opacity: 1, y: 0, duration: 1, ease: Power1.easeInOut }
-    );
-  }
-}, []);
-
-  useEffect(() => {
     fetchData();
   }, []);
+
+  useEffect(() => {
+    if(data && !loading){
+      setLoadContent(true);
+      setTimeout(() => {
+        if (element1Ref.current) {
+        gsap.fromTo(
+          element1Ref.current,
+          { opacity: 0, y: 100 },
+          { 
+            opacity: 1, y: 0, duration: .15, ease: Sine.easeIn,
+            onComplete: () => setLoadContent(false)
+          }
+        );
+      }
+      }, 500);
+    }
+  }, [loading]);
 
   async function fetchData() {
   setLoading(true);
@@ -41,15 +50,15 @@ export default function Home() {
     
     const responseData = response.data;
 
-    let loggedIn = false;
+    let loggedInSession = false;
     if (typeof window !== 'undefined') {
       const sessionInfo = getSessionData();
       if (sessionInfo) {
         const { sessionData } = sessionInfo;
 
-        loggedIn = responseData.logged_in && sessionData && Date.now() - sessionData.timestamp < 86400000; // 24 Hours / 60000 is 1 minute(for testing)
+        loggedInSession = responseData.logged_in && sessionData && Date.now() - sessionData.timestamp < 86400000; // 24 Hours / 60000 is 1 minute(for testing)
 
-        if (!loggedIn) {
+        if (!loggedInSession) {
           const sessionKeys = Object.keys(localStorage).filter(key => key.startsWith('myapp-session-'));
           if (sessionKeys.length > 0) {
             const latestSessionKey = sessionKeys.sort().reverse()[0];
@@ -65,7 +74,7 @@ export default function Home() {
 
 
 
-    setLoggedIn(loggedIn);
+    setLoggedIn(loggedInSession);
     setData(responseData.posts);
 
     setLoading(false);
@@ -120,36 +129,49 @@ export default function Home() {
         </div>
       )}
 
-      {/* spacing */}
-    {data.length > 0 && (
-      <div ref={element1Ref} 
-        className="
-          max-w-2xl 
-          mx-auto 
-          grid 
-          pt-4 
-          gap-x-6 
-          gap-y-4 
-          grid-cols-1 
-          sm:gap-y-6 
-          sm:pt-8 
-          sm:grid-cols-2
-          lg:mx-0 
-          lg:max-w-none 
-          lg:grid-cols-3">
-        {data.map(({ id, title, linkTag, imgHref, content_excerpt }) => (
-          <HomeListItem key={id} id={id} title={title} contentExcerpt={content_excerpt} linkTag={linkTag} imgHref={imgHref} loggedIn={loggedIn} />
-        ))}
+  {data.length > 0 && (
+    // gsap animation compensation
+    <div className={`${loadContent ? "hidden" : "block"}`}>
+      
+      {/* grid layout and spacing */}
+        <div ref={element1Ref} 
+          className="
+            max-w-2xl 
+            mx-auto 
+            grid 
+            pt-4 
+            gap-x-6 
+            gap-y-4 
+            grid-cols-1 
+            sm:gap-y-6 
+            sm:pt-8 
+            sm:grid-cols-2
+            lg:mx-0 
+            lg:max-w-none 
+            lg:grid-cols-3">
+          {data.map(({ id, title, linkTag, imgHref, content_excerpt }) => (
+            <HomeListItem key={id} id={id} title={title} contentExcerpt={content_excerpt} linkTag={linkTag} imgHref={imgHref} loggedIn={loggedIn} />
+          ))}
+        </div>
       </div>
     )}
+    
 
-    <div className="flex justify-center text-xl my-16">
-      <p className="cursor-pointer block my-0 bg-[#1A0123] px-8 text-lg text-white ease ease-in-out duration-300 hover:pl-12 hover:pr-12">
-        Load more...
-      </p>
-    </div>
-    </div>{/* \container */}
-    <Footer />
+<div className={`${loadContent ? "hidden" : "block"}`}>
+      <div className="flex justify-center text-xl my-16">
+        <p className="cursor-pointer block my-0 bg-[#1A0123] px-8 text-lg text-white ease ease-in-out duration-300 hover:pl-12 hover:pr-12">
+          Load more...
+        </p>
+      </div>
+      
+      </div>{/* \container */}
+
+</div>
+
+<div className={`${loadContent ? "hidden" : "block"}`}>
+      {data && <Footer />}
+</div>
+    
     </>
   );
 }
