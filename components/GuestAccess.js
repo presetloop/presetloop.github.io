83f4caeb588f5de255a113af4ea30812bd09c999
username-matthew <matthew@olk1.com>
2023-05-05 16:23:36 +0100

    configure responsive layout for guest pass CTA

--- components/GuestAccess.js
+++ components/GuestAccess.js
@@ -15,7 +15,7 @@ export default function GuestAccess({ setEmail, setPassword, setInputFieldType }
         <div className="flex flex-col sm:flex-row mt-4 gap-4">
          
           
-          <div className="bg-red-50 items-center">
+          <div className="bg-red-50 text-center items-center">
               <button
                 className="ml-1 text-slate-900"
                 onClick={handleCopy}>
@@ -27,7 +27,7 @@ export default function GuestAccess({ setEmail, setPassword, setInputFieldType }
             </button>
           </div>
          
-          <p className="text-slate-800">{'\u003C\u003C\u003C'} 24 Hour Guest Pass</p>
+          {window.innerWidth < 480 ? <p className="text-center text-slate-800">{'\u005E\u005E\u005E'} 24 Hour Guest Pass</p> : <p className="text-slate-800">{'\u003C\u003C\u003C'} 24 Hour Guest Pass</p>}
          </div>
       </div>
     </>
