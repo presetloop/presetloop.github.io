2116d31c038008d9c382c05adf9af1da2ed66873
username-matthew <matthew@olk1.com>
2023-06-10 13:29:47 +0100

    tweak element spacing and brand strapline

--- components/Footer.js
+++ components/Footer.js
@@ -7,18 +7,19 @@ export default function Footer() {
       <div className="loadedAni bg-[#101010] px-8 sm:px-0 py-8 sm:pt-36 sm:pb-44 flex flex-col text-center">
  
           
-          {/* LOGO */}
+            {/* LOGO */}
             <div className="flex justify-center">
               <img className="rounded-full my-8 h-40 w-40 sm:h-48 sm:w-48" src="/logo.svg" alt="Preset Loop" />
             </div>
   
-            <div className="mb-10">
-              <p className="text-white mb-28">{`${process.env.NEXT_PUBLIC_STRAPLINE}`}</p>
+
+            <div className="my-8">
+              
+              <p className="text-white mt-8 mb-24">{`${process.env.NEXT_PUBLIC_STRAPLINE}`}</p>
+
               <FooterSocials />
             </div>
           
-          
-
 
           <div className="my-16 flex flex-col gap-x-4 text-white tracking-tighter leading-8">
             <p>E.U.L.A.</p>
