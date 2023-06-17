e92538e0753e17be6e49c78492373c420366a9ca
username-matthew <matthew@olk1.com>
2023-06-18 00:25:12 +0100

    refactor to try catch

--- helpers/IsGuestContext.js
+++ helpers/IsGuestContext.js
@@ -4,17 +4,39 @@ const IsGuestContext = createContext();
 
 export const IsGuestProvider = ({ children }) => {
  
- const initialIsGuest = typeof window !== 'undefined' ? localStorage.getItem('isGuest') : null;
+//  const initialIsGuest = typeof window !== 'undefined' ? localStorage.getItem('isGuest') : null;
  
+let initialIsGuest;
+
+try {
+  initialIsGuest = typeof window !== 'undefined' ? localStorage.getItem('isGuest') : null;
+} catch (error) {
+  // Handle the security error
+  initialIsGuest = null;
+  console.error('Error accessing localStorage:', error);
+}
+
  const [isGuest, setIsGuest] = useState(initialIsGuest ? JSON.parse(initialIsGuest) : false);
 
  const [hasMounted, setHasMounted] = useState(false);
 
- useEffect(() => {
-  if (typeof window !== 'undefined') {
-    localStorage.setItem('isGuest', JSON.stringify(isGuest));
+//  useEffect(() => {
+//   if (typeof window !== 'undefined') {
+//     localStorage.setItem('isGuest', JSON.stringify(isGuest));
+//   }
+//  }, [isGuest]);
+
+useEffect(() => {
+  try {
+    if (typeof window !== 'undefined') {
+      localStorage.setItem('isGuest', JSON.stringify(isGuest));
+    }
+  } catch (error) {
+    // Handle the security error
+    console.error('Error accessing localStorage:', error);
   }
- }, [isGuest]);
+}, [isGuest]);
+
 
  useEffect(() => {
   setHasMounted(true);
