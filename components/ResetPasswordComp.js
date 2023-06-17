0270be8725f4804fd3ad3899d87173bcf784e2eb
username-matthew <matthew@olk1.com>
2023-06-17 11:50:56 +0100

    configure UX/hide form elements on successful password reset

--- components/ResetPasswordComp.js
+++ components/ResetPasswordComp.js
@@ -11,8 +11,9 @@ function ResetPasswordComp({ apiUrl }) {
 
     const [email, setEmail] = useState('');
     const [newPassword, setNewPassword] = useState('');
-    const [data, setData] = useState('');
     const [errorMessage, setErrorMessage] = useState('');
+    const [data, setData] = useState('');
+    const [retry, setRetry] = useState('');
     const [loading, setLoading] = useState(false);
 
     // to get input autofocus working
@@ -76,7 +77,8 @@ function ResetPasswordComp({ apiUrl }) {
         
         // Set the message returned by the PHP function
         if(JSON.parse(responseData) === "No user found with that email address or token."){
-          setData('Please try again or check you are using the latest email link');
+          setRetry('Please try again or check you are using the latest email link');
+          setData("");
           setErrorMessage(JSON.parse(responseData));
           setLoading(false);
           return; 
@@ -84,6 +86,7 @@ function ResetPasswordComp({ apiUrl }) {
         // Set the message returned by the PHP function
         // success, password reset
         setData(JSON.parse(responseData));
+        setRetry("");
         setErrorMessage('');
         setEmail('');
         setNewPassword('');
@@ -106,7 +109,7 @@ function ResetPasswordComp({ apiUrl }) {
               <input 
                 ref={titleField}
                 required 
-                className="appearance-none border-slate-900 mb-4 border-b-[1px] w-full py-2 px-0 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                className={`${data && "hidden"} appearance-none border-slate-900 mb-4 border-b-[1px] w-full py-2 px-0 text-gray-700 leading-tight focus:outline-none focus:shadow-outline`}
                 placeholder="Enter your email"
                 type="email" 
                 value={email} 
@@ -118,7 +121,7 @@ function ResetPasswordComp({ apiUrl }) {
             <div className="relative">
               <input 
                 required 
-                className="appearance-none border-slate-900 mb-4 border-b-[1px] w-full py-2 px-0 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                className={`${data && "hidden"} appearance-none border-slate-900 mb-4 border-b-[1px] w-full py-2 px-0 text-gray-700 leading-tight focus:outline-none focus:shadow-outline`}
                 placeholder="Enter a new password"
                 type="password" 
                 value={newPassword} 
@@ -126,13 +129,14 @@ function ResetPasswordComp({ apiUrl }) {
                 pattern="^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$"
                 />
             
-            {data && <p className="mt-2 text-blue-500 absolute -bottom-[10px] left-0 mb-0 text-md">{data}</p>}
+            {data && <p className="mt-2 text-blue-500 absolute -bottom-[10px] left-0 mb-0 text-xl">{data}</p>}
+            
             {errorMessage && <p className="absolute -bottom-[35px] left-0 mb-0 text-red-500 text-md">{errorMessage}</p>}
             </div>
 
-            <p className="mt-8 text-slate-400 text-sm">Password must be a minimum of 8 characters containing one uppercase letter, one lowercase letter, at least one number and one special character (e.g., !, @, #, $, %, ^, &, *)</p>
+            <p className={`${data && "hidden"} mt-8 text-slate-400 text-sm`}>Password must be a minimum of 8 characters containing one uppercase letter, one lowercase letter, at least one number and one special character (e.g., !, @, #, $, %, ^, &, *)</p>
 
-            <button className="mt-4 border-slate-900 border-2 sm:hover:bg-slate-900 sm:hover:text-white font-bold py-0 px-4 text-lg focus:outline-none focus:shadow-outline ease-in-out duration-150" type="submit">
+            <button className={`${data && "hidden"} mt-4 border-slate-900 border-2 sm:hover:bg-slate-900 sm:hover:text-white font-bold py-0 px-4 text-lg focus:outline-none focus:shadow-outline ease-in-out duration-150`} type="submit">
               {loading ? 'Reseting...' : 'Reset Password'}
             </button>
         </form>
