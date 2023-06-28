e345eaeee8b8f6fcdbbdaccad566a73f229c525b
username-matthew <matthew@olk1.com>
2023-06-28 20:39:05 +0100

    re-add random colour class helper

---
+++ helpers/getRandomColourClass.js
@@ -0,0 +1,22 @@
+export default  function getRandomColourClass(classesArray) {
+
+  const randomIndex = Math.floor(Math.random() * classesArray.length);
+    return classesArray[randomIndex];
+  }
+
+
+export const random50 = ["bg-cyan-50", "bg-rose-50", "bg-fuchsia-50", "bg-emerald-50", "bg-teal-50", "bg-sky-50", "bg-violet-50", "bg-lime-50", "bg-amber-50"];
+
+export const random100 = ["bg-cyan-100", "bg-rose-100", "bg-fuchsia-100", "bg-emerald-100", "bg-teal-100", "bg-sky-100", "bg-violet-100", "bg-lime-100", "bg-amber-100"];
+  
+export const random300 = ["bg-cyan-300", "bg-rose-300", "bg-fuchsia-300", "bg-emerald-300", "bg-teal-300", "bg-sky-300", "bg-violet-300", "bg-lime-300", "bg-amber-300"];
+  
+export const random400 = ["bg-cyan-400", "bg-rose-400", "bg-fuchsia-400", "bg-emerald-400", "bg-teal-400", "bg-sky-400", "bg-violet-400", "bg-lime-400", "bg-amber-400"];
+
+
+
+ // const og = ["blue-300", "red-50", "pink-400", "yellow-100", "red-100", "green-100"];
+// export const random50 = ["bg-blue-50", "bg-red-50", "bg-pink-50", "bg-yellow-50", "bg-green-50", "bg-purple-50", "bg-indigo-50", "bg-gray-50"];
+// export const random100 = ["bg-blue-100", "bg-red-100", "bg-pink-100", "bg-yellow-100", "bg-green-100", "bg-purple-100", "bg-indigo-100", "bg-gray-100"];
+// export const random300 = ["bg-blue-300", "bg-red-300", "bg-pink-300", "bg-yellow-300", "bg-green-300", "bg-purple-300", "bg-indigo-300", "bg-gray-300"];
+// export const random400 = ["bg-blue-400", "bg-red-400", "bg-pink-400", "bg-yellow-400", "bg-green-400", "bg-purple-400", "bg-indigo-400", "bg-gray-400"];  
